9d5651d96c24d6f4f07f7e4a634d81fb46cf1db8
Pilo11 <10966593+Pilo11@users.noreply.github.com>
2022-03-17 12:37:32 +0100

    Added .net 6 compatible initial code

---
+++ src/AsmJitter/Model/Instruction/Label.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsmJitter.Model.Instruction
+{
+    public class Label : AbstractInstruction
+    {
+
+        public string Name { get; }
+
+        public Label(string name, Code code)
+        {
+            Name = name;
+            code.AddLabel(this);
+        }
+
+        public override IEnumerable<byte> GetBytes()
+        {
+            return Enumerable.Empty<byte>();
+        }
+
+    }
+}
